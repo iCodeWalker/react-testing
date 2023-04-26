e8084f51e7e7b102fb9b89bd880a9d55a63e0cc7
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-04-27 01:19:41 +0530

    Added: Grouping test and it method

--- src/components/greet/greetUser.test.tsx
+++ src/components/greet/greetUser.test.tsx
@@ -4,15 +4,68 @@
 import { render, screen } from "@testing-library/react";
 import GreetUser from "./greetUser";
 
-test("Greet renders correctly", () => {
-  render(<GreetUser />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
+// test("Greet renders correctly", () => {
+//   render(<GreetUser />);
+//   const textElement = screen.getByText("Hello");
+//   expect(textElement).toBeInTheDocument();
+// });
+
+// To run a specific test
+// --> test.only("Greet render with name", () => {
+// then only this test is going to run from this file
+
+// To skip a specfic test
+// --> test.skip("Greet render with name", () => {
+// using skip --> than this test will be skipped
+
+// test("Greet render with name", () => {
+//   render(<GreetUser name="Vaibhav" />);
+
+//   const textElement = screen.getByText("Hello Vaibhav");
+//   expect(textElement).toBeInTheDocument();
+// });
+
+// Grouping tests
+
+// 1. describe method is used to group tests together.
+// 2. can use .skip or .only with describe method
+// 3 .decribe can be nested.
+// 4. can add multiple describe blocks inside a single file
+
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<GreetUser />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("render with name", () => {
+    render(<GreetUser name="Vaibhav" />);
+
+    const textElement = screen.getByText("Hello Vaibhav");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  // --> Nested describe method
+
+  //   describe("Nested", () => {
+  //     test("render with name", () => {
+  //       render(<GreetUser name="Vaibhav" />);
+
+  //       const textElement = screen.getByText("Hello Vaibhav");
+  //       expect(textElement).toBeInTheDocument();
+  //     });
+  //   });
 });
 
-test("Greet render with name", () => {
-  render(<GreetUser name="Vaibhav" />);
+// --> Alternative of test method is "it"
 
-  const textElement = screen.getByText("Hello Vaibhav");
-  expect(textElement).toBeInTheDocument();
-});
+//   it("render with name", () => {
+//     render(<GreetUser name="Vaibhav" />);
+
+//     const textElement = screen.getByText("Hello Vaibhav");
+//     expect(textElement).toBeInTheDocument();
+//   });
+
+// --> To replicate test.only we can use "fit" == focuses on a particular test
+// --> To replicate test.skip we can use "xit" == skips or excludes a particular test
