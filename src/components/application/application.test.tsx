13a3d5f14034715a31386657aa6c7097d72b3e8c
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-04-27 16:54:25 +0530

    Added: more query methods

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -15,13 +15,42 @@ describe("Application", () => {
     const sectionHeading = screen.getByRole("heading", { level: 2 });
     expect(sectionHeading).toBeInTheDocument();
 
+    // getByText() query method --> used for para, div, span
+
+    const paragraphElement = screen.getByText("All fields are mandatory");
+    expect(paragraphElement).toBeInTheDocument();
+
+    // getAllByAltText() query method --> used for elements that has alt attributes like image, input, textarea or custom components
+
+    const imageElement = screen.getByAltText("a person with a laptop");
+    expect(imageElement).toBeInTheDocument();
+
+    // getByTitle query method --> elements that has matching title attribute
+
+    const closeElement = screen.getByTitle("close");
+    expect(closeElement).toBeInTheDocument();
+
+    // getByTestId query method --> returns an element that has matching "data-testid" attr.
+
+    const customElement = screen.getByTestId("custom-element");
+    expect(customElement).toBeInTheDocument();
+
     const inputElement = screen.getByRole("textbox", { name: "Name" });
     expect(inputElement).toBeInTheDocument();
 
+    // getByLabelText() query method
     const inputElement2 = screen.getByLabelText("Name", {
       selector: "input", // takes the type of element when encounters more than one with same label
     });
     expect(inputElement2).toBeInTheDocument();
+
+    // getByPlaceholderText() query method
+    const inputElement3 = screen.getByPlaceholderText("Fullname");
+    expect(inputElement3).toBeInTheDocument();
+
+    // getByDisplayValue() query method
+    const inputElement4 = screen.getByDisplayValue("Vaibhav");
+    expect(inputElement4).toBeInTheDocument();
 
     const bioElement = screen.getByRole("textbox", { name: "Bio" });
     expect(bioElement).toBeInTheDocument();
@@ -32,6 +61,7 @@ describe("Application", () => {
     const termsElement = screen.getByRole("checkbox");
     expect(termsElement).toBeInTheDocument();
 
+    // getByLabelText() query method
     const termsElement2 = screen.getByLabelText(
       "I agree to the terms and conditions"
     );
@@ -41,3 +71,14 @@ describe("Application", () => {
     expect(submitBtnElement).toBeInTheDocument();
   });
 });
+
+// Priority order for query methods
+
+// 1. getByRole
+// 2. getByLabelText
+// 3. getByPlaceholderText
+// 4. getByText
+// 5. getByDisplayValue
+// 6. getByAltText
+// 7. getByTitle
+// 8. getByTestId
