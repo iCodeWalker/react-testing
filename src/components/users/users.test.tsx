3392cfa1c9b20a5f7a718f0c4aa6aabd49beffd9
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-05-04 18:42:04 +0530

    Added: MSW Error handling

--- src/components/users/users.test.tsx
+++ src/components/users/users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Users } from "./users";
+import { server } from "../../mocks/server";
+import { rest } from "msw";
 
 describe("Users API call", () => {
   test("renders correctly", () => {
@@ -15,6 +17,26 @@ describe("Users API call", () => {
     render(<Users />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(4);
+  });
+
+  // test for error in api call
+  test("renders error", async () => {
+    // we need to reset the handler that we defined in the main file to throw an error
+    // for that we need to get hold of server from server.ts
+    server.use(
+      rest.get(
+        "https://jsonplaceholder.typicode.com/users",
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+
+    // after reseting the handler, reders the user component
+
+    render(<Users />);
+    const errorText = await screen.findByText("Error fetching users");
+    expect(errorText).toBeInTheDocument();
   });
 });
 
