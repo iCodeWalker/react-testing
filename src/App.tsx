0a6c36834de3ad6fd37d0640eabedd6b38710dcf
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-05-04 16:46:45 +0530

    Added: setup of mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.css";
 import { Application } from "./components/application/application";
+import { MuiMode } from "./components/mui/muiMode";
 import HomePage from "./pages/homePage";
+import { AppProviders } from "./providers/appProvider";
 
 function App() {
   return (
-    <div className="App">
-      <Application />
-      <HomePage />
-    </div>
+    // <div className="App">
+    //   <Application />
+    //   <HomePage />
+    // </div>
+    <AppProviders>
+      <div className="App">
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
